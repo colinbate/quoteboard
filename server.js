2c9daae8abd32738a7567c93e421c588eef28cc1
Colin Bate <colin@colinbate.com>
2012-08-20 00:34:04 -0300

    Separated models on the server and added a last ID concept to know if new quotes have arrived since you last fetched the page.

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var dateformat = require('dateformat');
-//var io = require('socket.io').listen(server);
+var quotes = require('./lib/quotelist');
 
 app.configure(function () {
         app.set('views', __dirname + '/views');
@@ -11,59 +10,26 @@ app.configure(function () {
         app.use(express.bodyParser());
 });
 
-var Quote = function(saying, author, day) {
-	this.saying = saying;
-	this.author = author;
-	if (!day) {
-		day = new Date();
-	}
-	if (!(day instanceof Date)) {
-		day = new Date(day);
-	}
-	this.day = day;
-	this.displayDate = dateformat(day, 'mediumDate');
-}
-
-Quote.prototype.isValid = function () {
-	return !!this.saying && !!this.author;
-};
-
-var quoteList = [];
-
 app.get('/', function(req, res){
   res.render('index', {title: 'Quote Board'});
 });
 
 app.get('/quotes', function(req, res) {
-	res.send({ quotes: quoteList });
+	res.send({ quotes: quotes.board.asArray(), lastId: quotes.board.lastId() });
 });
 
 app.post('/quotes', function(req, res) {
 	if(req.body) {
-		var q = new Quote(req.body.saying, req.body.author, req.body.day);
+		var q = new quotes.Quote(req.body.saying, req.body.author, req.body.day);
+		var lastId = req.body.lastId;
 		if (q.isValid()) {
-			quoteList.unshift(q);
-			res.send({ quote: q });
+			var newQuotes = quotes.board.getSince(lastId, q);
+			var lastLastId = quotes.board.add(q);
+			res.send({ quotes: newQuotes, lastId: lastLastId });
 		} else {
 			res.send({ err: { message: 'Quote is not valid' } });
 		}
 	}
 });
-
-/* Going to leave this here for informational purposes.
-
-io.configure(function() {
-	io.set("transports", ["xhr-polling"]);
-	io.set("polling duration", 10);
-	io.set("log level", 2);
-});
-io.sockets.on('connection', function(socket) {
-    socket.on('saveQuote', function(data) {
-        var q = new Quote(data.saying, data.author, data.day);
-        quoteList.unshift(q);
-        io.sockets.emit('newQuote', { quote: q });
-    });
-});
-*/
 
 server.listen(process.env.PORT || 3000);
